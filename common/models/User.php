35bfb32780b200198e509a424b1e7614244a03cf
jkofsky <jkofsky@cox.net>
2016-07-19 01:44:31 -0500

    Modification of the User Model & Added 5 status models

--- common/models/User.php
+++ common/models/User.php
@@ -6,9 +6,15 @@ use Yii;
 use yii\base\NotSupportedException;
 use yii\behaviors\TimestampBehavior;
 use yii\db\ActiveRecord;
-use yii\db\Expression;
 use yii\web\IdentityInterface;
+use yii\helpers\ArrayHelper;
 use yii\helpers\Security;
+use yii\helpers\Url;
+use yii\helpers\Html;
+use common\models\Profile;
+use backend\models\Role;
+use backend\models\Status;
+use backend\models\UserType;
 
 /**
  * User model
@@ -62,17 +68,20 @@ class User extends ActiveRecord implements IdentityInterface {
      */
     public function rules() {
         return [
-            ['status_id', 'default', 'value' => self::STATUS_ACTIVE],
-            ['role_id', 'default', 'value' => 1],
-            ['user_type_id', 'default', 'value' => 1],
-            ['username', 'filter', 'filter' => 'trim'],
-            ['username', 'required'],
-            ['username', 'unique'],
-            ['username', 'string', 'min' => 2, 'max' => 255],
-            ['email', 'filter', 'filter' => 'trim'],
-            ['email', 'required'],
-            ['email', 'email'],
-            ['email', 'unique'],
+            [['status_id'], 'default', 'value' => self::STATUS_ACTIVE],
+            [['status_id'], 'in', 'range' => array_keys($this->getStatusList())],
+            [['role_id'], 'default', 'value' => 1],
+            [['role_id'], 'in', 'range' => array_keys($this->getRoleList())],
+            [['user_type_id'], 'default', 'value' => 1],
+            [['user_type_id'], 'in', 'range' => array_keys($this->getUserTypeList())],
+            [['username'], 'filter', 'filter' => 'trim'],
+            [['username'], 'required'],
+            [['username'], 'unique'],
+            [['username'], 'string', 'min' => 2, 'max' => 255],
+            [['email'], 'filter', 'filter' => 'trim'],
+            [['email'], 'required'],
+            [['email'], 'email'],
+            [['email'], 'unique'],
         ];
     }
 
@@ -88,6 +97,15 @@ class User extends ActiveRecord implements IdentityInterface {
             'role_id' => \Yii::t('app', 'Role'),
             'status_id' => \Yii::t('app', 'Status'),
             'user_type_id' => \Yii::t('app', 'User Type'),
+            'roleName' => Yii::t('app', 'Role'),
+            'statusName' => Yii::t('app', 'Status'),
+            'profileId' => Yii::t('app', 'Profile'),
+            'profileLink' => Yii::t('app', 'Profile'),
+            'userLink' => Yii::t('app', 'User'),
+            'username' => Yii::t('app', 'User'),
+            'userTypeName' => Yii::t('app', 'User Type'),
+            'userTypeId' => Yii::t('app', 'User Type'),
+            'userIdLink' => Yii::t('app', 'ID'),
             'created_at' => \Yii::t('app', 'Created'),
             'created_by' => \Yii::t('app', 'Created By'),
             'updated_at' => \Yii::t('app', 'Last Updated'),
@@ -215,12 +233,125 @@ class User extends ActiveRecord implements IdentityInterface {
     }
 
     /**
+     * get role relationship
+     *
+     */
+    public function getRole() {
+        return $this->hasOne(Role::className(), ['id' => 'role_id']);
+    }
+
+    /**
+     * get role name
+     *
+     */
+    public function getRoleName() {
+        return $this->role ? $this->role->role_name : '- no role -';
+    }
+
+    /**
+     * get list of roles for dropdown
+     */
+    public static function getRoleList() {
+        $droptions = Role::find()->asArray()->all();
+        return ArrayHelper::map($droptions, 'id', 'role_name');
+    }
+
+    /**
+     * get status relation
+     */
+    public function getStatus() {
+        return $this->hasOne(Status::className(), ['id' => 'status_id']);
+    }
+
+    /**
+     * get status name
+     */
+    public function getStatusName() {
+        return $this->status ? $this->status->status_name : '- no status -';
+    }
+
+    /**
+     * get list of statuses for dropdown
+     */
+    public static function getStatusList() {
+        $droptions = Status::find()->asArray()->all();
+        return ArrayHelper::map($droptions, 'id', 'status_name');
+    }
+
+    /**
      *  Link to Profile Model
      * 
      * @return mixed
      */
     public function getProfile() {
         return $this->hasOne(Profile::className(), ['user_id' => 'id']);
+    }
+
+    /**
+     * @getProfileId
+     * 
+     */
+    public function getProfileId() {
+        return $this->profile ? $this->profile->id : 'none';
+    }
+
+    /**
+     * @getProfileLink
+     * 
+     */
+    public function getProfileLink() {
+        $url = Url::to(['profile/view', 'id' => $this->profileId]);
+        $options = [];
+        return Html::a($this->profile ? 'profile' : 'none', $url, $options);
+    }
+
+    /**
+     * getUserType
+     */
+    public function getUserType() {
+        return $this->hasOne(UserType::className(), ['id' => 'user_type_id']);
+    }
+
+    /**
+     * get user type name
+     */
+    public function getUserTypeName() {
+        return $this->userType ? $this->userType->user_type_name : '- no user type -';
+    }
+
+    /**
+     * get list of user types for dropdown
+     */
+    public static function getUserTypeList() {
+        $droptions = UserType::find()->asArray()->all();
+        return ArrayHelper::map($droptions, 'id', 'user_type_name');
+    }
+
+    /**
+     * get user type id
+     */
+    public function getUserTypeId() {
+        return $this->userType ? $this->userType->id : 'none';
+    }
+
+    /**
+     * get user id Link
+     *
+     */
+    public function getUserIdLink() {
+        $url = Url::to(['user/update', 'id' => $this->id]);
+        $options = [];
+        return Html::a($this->id, $url, $options);
+    }
+
+    /**
+     * @getUserLink
+     * 
+     */
+    public function getUserLink() {
+        $url = Url::to(['user/view', 'id' => $this->id]);
+        $options = [];
+        return Html::a($this->username, $url, $options);
     }
 
 }
