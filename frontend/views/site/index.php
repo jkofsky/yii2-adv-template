c45fccdfb026f170980e01c0dc7e5cf93fc69402
jkofsky <jkofsky@cox.net>
2017-06-09 23:29:56 -0500

    Modified Index.php

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,7 +1,5 @@
 <?php
 
-use \yii\bootstrap\Modal;
-use \yii\bootstrap\Collapse;
 use \yii\bootstrap\Alert;
 use yii\helpers\Html;
 use kartik\social\TwitterPlugin;
@@ -11,6 +9,14 @@ use kartik\social\TwitterPlugin;
 $this->title = 'My Yii2 Template';
 ?>
 <div class="site-index">
+    <?Php
+    echo Alert::widget([
+        'options' => [
+            'class' => 'alert-danger',
+        ],
+        'body' => 'Information on this site is for <span style="font-weight:bolder;">Internal Use Only!</span>',
+    ]);
+    ?> 
 
     <div class="jumbotron">
         <?php
@@ -30,121 +36,59 @@ $this->title = 'My Yii2 Template';
         ?> 
     </div>
 
-    <?php
-    echo Collapse::widget([
+    <div class="body-content">
 
-        'items' => [
-            [
-                'label' => 'Tweeter Feed',
-                'content' => TwitterPlugin::widget([
+        <div class="row">
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">It's Free</div>
+                    <div class="panel-body">
+                        <p>
+                            <?php
+                            if (!Yii::$app->user->isGuest) {
+
+                                echo Yii::$app->user->identity->username . ' is doing cool stuff. ';
+                            }
+                            ?> 
+                            Starting with this free, open source Yii 2 template and it will save you 
+                            a lot of time.  You can deliver projects to the customer quickly, with 
+                            a  lot of boilerplate already taken care of for you, so you can concentrate 
+                            on the complicated stuff.</p>
+                        <p>
+                            <a class="btn btn-default" href="http://www.yiiframework.com/doc-2.0/guide-index.html">Yii Documentation &raquo;</a>
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <?php
+                echo TwitterPlugin::widget([
                     'type' => TwitterPlugin::TIMELINE,
                     'screenName' => 'catcountry987',
                     'settings' => [
                         'widget-id' => '293376767144632320',
                         'size' => 'large'
                     ],
-                    //'options' => [ 'width' => '100%']
-                ]),
-                // open its content by default
-                'contentOptions' => ['class' => 'in']
-            ],
-            // another group item
-            [
-                'label' => 'Top Features',
-                'content' => 'FacebookPlugin::widget([1]',
-//                    'label' => 'Top Features',
-//                    'content' => FacebookPlugin::widget([
-//                        'type' => FacebookPlugin::SHARE,
-//                        'settings' => ['href' => 'http://www.yii2build.com', 'width' => '350']
-//                    ]),
-            ],
-        ]
-    ]);
-
-    Modal::begin([
-        'header' => '<h2>Latest Comments</h2>',
-        'toggleButton' => ['label' => 'comments'],
-    ]);
-
-//        echo FacebookPlugin::widget([
-//            'type' => FacebookPlugin::COMMENT,
-//            'settings' => ['href' => 'http://www.yii2build.com', 'width' => '350']
-//        ]);
-
-    Modal::end();
-    ?>
-
-    <br/>
-    <br/>
-
-    <?Php
-    echo Alert::widget([
-        'options' => [
-            'class' => 'alert-info',
-        ],
-        'body' => 'Launch your project like a rocket...',
-    ]);
-    ?> 
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Free</h2>
-
-                <p>
-                    <?php
-                    if (!Yii::$app->user->isGuest) {
-
-                        echo Yii::$app->user->identity->username . ' is doing cool stuff. ';
-                    }
-                    ?> 
-                    Starting with this free, open source Yii 2 template and it will save you 
-                    a lot of time.  You can deliver projects to the customer quickly, with 
-                    a  lot of boilerplate already taken care of for you, so you can concentrate 
-                    on the complicated stuff.</p>
-                <p>
-                    <a class="btn btn-default" href="http://www.yiiframework.com/doc-2.0/guide-index.html">Yii Documentation &raquo;</a>
-                </p>
-
-                <?php
-//                    echo FacebookPlugin::widget([
-//                        'type' => FacebookPlugin::LIKE,
-//                        'settings' => []
-//                    ]);
-                ?> 
+                        //'options' => [ 'width' => '100%']
+                ]);
+                ?>
             </div>
-            <div class="col-lg-4">
-                <h2>Advantages</h2>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Code Quick, Code Right!</div>
+                    <div class="panel-body">
 
-                <p>
-                    Ease of use is a huge advantage.  We've simplifiled RBAC and given you Free/Paid
-                    user type out of the box.  The Social plugins are so quick and easy to install, 
-                    you will love it!
-                </p>
+                        <p>Leverage the power of the awesome Yii 2 framework with this enhanced template.  
+                            Based Yii 2's advanced template, you get a full frontend and backend 
+                            implementation that features rich UI for backend management.
+                        </p>
 
-                <p>
-                    <a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a>
-                </p>
-
-                <?php
-//                    echo FacebookPlugin::widget([
-//                        'type' => FacebookPlugin::COMMENT,
-//                        'settings' => ['href' => 'http://www.yii2build.com', 'width' => '350']
-//                    ]);
-                ?> 
-            </div>
-            <div class="col-lg-4">
-                <h2>Code Quick, Code Right!</h2>
-
-                <p>Leverage the power of the awesome Yii 2 framework with this enhanced template.  
-                    Based Yii 2's advanced template, you get a full frontend and backend 
-                    implementation that features rich UI for backend management.
-                </p>
-
-                <p>
-                    <a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a>
-                </p>
+                        <p>
+                            <a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a>
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
